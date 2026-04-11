cd77c0da3b4d876c7d76a1f327d10629606b596a
theintrospectiveidiot <pbhaji57@gmail.com>
2026-04-11 17:04:57 +0530

    organised stuff

--- actual_file.c
+++ actual_file.c
@@ -1,21 +1,25 @@
 #include "interesting.h"
 
 int main() {
-	int *numbrs = init(2);
+	printf("dim >> ");
+	int dim;
+	scanf("%d",&dim);
+	int *numbrs = init(dim,(int[]){3,3,3});
 
 	push(numbrs,1,2,3);
 	push(numbrs,4,5);
+	push(numbrs,6,7,8,9);
 
 	god_stuff *headr = (god_stuff *)numbrs - 1;
 	/*for (int k=0;k<((god_stuff *)numbrs - 1)->traverse[0];k++) {
 		for(int j=0;j<((god_stuff *)numbrs - 1)->traverse[1];j++) {
 			for(int i=0;i<((god_stuff *)numbrs - 1)->traverse[2];i++) {
-				printf("numbrs[%d][%d][%d] = %d\t",k,j,i,numbrs[k*(((god_stuff *)numbrs - 1)->traverse[2]*((god_stuff *)numbrs - 1)->traverse[1]) + j*(((god_stuff *)numbrs - 1)->traverse[2]) + i]);
+				printf("numbrs[%d][%d][%d] = %d\t",k,j,i,numbrs[k*headr->strides[0] + j*headr->strides[1] + i*headr->strides[2]]);
 			}
 			printf("\n");
 		}
 		printf("\n\n");
 	}*/
 
-	printf("desired num = %d\n",summon(numbrs,1,1));
+	printf("desired num = %d\n",summon(numbrs,0,1,1));
 }
